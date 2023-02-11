cbf96c5fe703706ce2308ed7d387fba1284a1622
edakaraman <95571155+edakaraman@users.noreply.github.com>
2023-02-11 17:59:48 +0300

    The table section where the data of the selected companies is displayed has been added.

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -7,7 +7,7 @@ import { WatchListContext } from "../context/watchListContext";
 
 export const StockList = () => {
     const [stock,setStock] = useState([]);
-    const {watchList} = useContext(WatchListContext);
+    const {watchList,deleteStock} = useContext(WatchListContext);
     const navigate = useNavigate();
 
    const renderIcon = (data) => {
@@ -74,14 +74,14 @@ export const StockList = () => {
                             <td> {stockData.data.h} </td>
                             <td> {stockData.data.l} </td>
                             <td> {stockData.data.o} </td>
-                            <td> {stockData.data.pc} </td>
+                            <td> {stockData.data.pc} <button className="btn btn-danger btn-sm ml-3 d-inline-block delete-button" onClick={(e) => {
+                                e.stopPropagation();
+                                deleteStock(stockData.symbol)
+                            }}> Remove</button></td>
                         </tr>
                     )
                    })}
                 </tbody>
-
-
-
             </table>
         </div>
     )
