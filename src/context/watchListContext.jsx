7a690e93a41204ecbc2964bdfe4f29216a3ebcb9
edakaraman <95571155+edakaraman@users.noreply.github.com>
2023-03-08 00:36:02 +0300

    Update watchListContext.jsx

--- src/context/watchListContext.jsx
+++ src/context/watchListContext.jsx
@@ -1,5 +1,6 @@
 import React,{createContext,useState,useEffect} from "react";
 
+
 export const WatchListContext = createContext();
 //["GOOGL","MSFT","AMZN"]
 export const WatchListContextProvider = (props) => {
